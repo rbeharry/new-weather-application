4a4d25e10d9f66f06c2894436a314e9f1d67fc62
rbeharry <rlbeharry@hotmail.com>
2025-10-03 20:14:46 -0400

    Added search functionality using API

--- src/index.js
+++ src/index.js
@@ -1,10 +1,33 @@
+function refreshWeather (response) {
+    let temperatureElement = document.querySelector("#temperature");
+let cityElement = document.querySelector("#city");
+cityElement.innerHTML = response.data.city;
+    let temperature=response.data.temperature.current;
+
+    temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+
+}
+
+
+function searchCity (city) {
+    let apiKey = "3fbc16ffbdt190o64c90aa576419eec5"
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`
+axios.get(apiUrl).then(refreshWeather);
+}
+
+
+
 
 function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
-let cityElement = document.querySelector("#city");
-cityElement.innerHTML= searchInput.value;
+
+
+searchCity(searchInput.value);
 
 }
 let searchFormElement = document.querySelector("#search-form");
+
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("New York");
