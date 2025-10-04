2d930c418eb833117e6a28388618983f62c11b75
rbeharry <rlbeharry@hotmail.com>
2025-10-04 05:29:05 -0400

    Injected data for icons

--- src/index.js
+++ src/index.js
@@ -1,24 +1,25 @@
-function refreshWeather (response) {
-    let temperatureElement = document.querySelector("#temperature");
-let cityElement = document.querySelector("#city");
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#weather-app-icon");
 
-let descriptionElement = document.querySelector("#description");
-let humidityElement = document.querySelector("#humidity");
-let windSpeedElement = document.querySelector("#wind-speed");
-let timeElement = document.querySelector("#time");
-let date = new Date(response.data.time * 1000);
-cityElement.innerHTML = response.data.city;
-    let temperature=response.data.temperature.current;
-
-
-
-    
-temperatureElement.innerHTML = Math.round(response.data.temperature.current);
-descriptionElement.innerHTML = response.data.condition.description;
-humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
-windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
-timeElement.innerHTML = `${formatDate(date)}`;
+  cityElement.innerHTML = response.data.city;
+  timeElement.innerHTML = formatDate(date);
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  temperatureElement.innerHTML = Math.round(temperature);
+  iconElement.innerHTML =`<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
+console.log (response.data);
 }
+
+
 
 function formatDate(date) {
   let minutes = date.getMinutes();
